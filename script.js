9b3879ad2fcbd90b321f306bfc76d02a48945880
Fahad849 <fahadsarkarwa@gmail.com>
2026-05-16 06:53:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const taskInput = document.getElementById("taskInput");
+
+const addBtn = document.getElementById("addBtn");
+
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", () => {
+
+    const taskText = taskInput.value;
+
+    if(taskText === ""){
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    li.textContent = taskText;
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+
+});
