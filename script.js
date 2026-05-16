62a9cd1758b1a20a8c663b8c67eabf369a4021fc
Fahad849 <fahadsarkarwa@gmail.com>
2026-05-16 07:02:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const taskInput = document.getElementById("taskInput");
-
-const addBtn = document.getElementById("addBtn");
-
-const taskList = document.getElementById("taskList");
-
-addBtn.addEventListener("click", () => {
-
-    const taskText = taskInput.value;
-
-    if(taskText === ""){
-        return;
-    }
-
     const li = document.createElement("li");
 
-    li.textContent = taskText;
+    const span = document.createElement("span");
+
+    span.textContent = taskText;
+
+    const deleteBtn = document.createElement("button");
+
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener("click", () => {
+
+        li.remove();
+
+    });
+
+    li.appendChild(span);
+
+    li.appendChild(deleteBtn);
 
     taskList.appendChild(li);
 
