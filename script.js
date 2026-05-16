008b548107ef09224010a754340708f6d7580399
Fahad849 <fahadsarkarwa@gmail.com>
2026-05-16 07:06:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+   const taskInput = document.getElementById("taskInput");
+
+const addBtn = document.getElementById("addBtn");
+
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", () => {
+
+    const taskText = taskInput.value;
+
+    if(taskText === ""){
+        return;
+    }
+
     const li = document.createElement("li");
 
     const span = document.createElement("span");
